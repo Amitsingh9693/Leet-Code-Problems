ad4a40f2d326d404d0811bdd17c71c1ea128d74f
Amit Kumar <149372234+Amitsingh9693@users.noreply.github.com>
2026-07-14 21:27:14 +0530

    Time: 870 ms (11.11%) | Memory: 47.3 MB (75.00%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -25,31 +25,53 @@ class Solution {
         int n=nums.length;
         int max=0;
         for(int i:nums) max=Math.max(i,max);
-        dp=new int[n+1][max+1][max+1];
+        // dp=new int[n+1][max+1][max+1];
         // for(int i=0;i<=n;i++){
         //     for(int j=0;j<=max;j++){
         //         Arrays.fill(dp[i][j],-1);
         //     }
         // }
         // return solve(nums,0,0,0);
+        // for(int i=0;i<=max;i++){
+        //     for(int j=0;j<=max;j++){
+        //         if(i==j && i!=0 && j!=0)
+        //         dp[n][i][j]=1;
+        //     }
+        // }
+
+        // for(int i=n-1;i>=0;i--){
+        //     for(int seq1=max;seq1>=0;seq1--){
+        //         for(int seq2=max;seq2>=0;seq2--){
+        //             long ans=0;
+        //             ans+=dp[i+1][seq1][seq2];
+        //             ans+=dp[i+1][gcd(nums[i],seq1)][seq2];
+        //             ans+=dp[i+1][seq1][gcd(nums[i],seq2)];
+        //             dp[i][seq1][seq2]=(int)(ans%1000000007);
+        //         }
+        //     }
+        // }
+        // return dp[0][0][0];
+        int[][] prev=new int[max+1][max+1];
         for(int i=0;i<=max;i++){
             for(int j=0;j<=max;j++){
                 if(i==j && i!=0 && j!=0)
-                dp[n][i][j]=1;
+                prev[i][j]=1;
             }
         }
 
         for(int i=n-1;i>=0;i--){
+            int[][] curr=new int[max+1][max+1];
             for(int seq1=max;seq1>=0;seq1--){
                 for(int seq2=max;seq2>=0;seq2--){
                     long ans=0;
-                    ans+=dp[i+1][seq1][seq2];
-                    ans+=dp[i+1][gcd(nums[i],seq1)][seq2];
-                    ans+=dp[i+1][seq1][gcd(nums[i],seq2)];
-                    dp[i][seq1][seq2]=(int)(ans%1000000007);
+                    ans+=prev[seq1][seq2];
+                    ans+=prev[gcd(nums[i],seq1)][seq2];
+                    ans+=prev[seq1][gcd(nums[i],seq2)];
+                    curr[seq1][seq2]=(int)(ans%1000000007);
                 }
             }
+            prev=curr;
         }
-        return dp[0][0][0];
+        return prev[0][0];
     }
 }
